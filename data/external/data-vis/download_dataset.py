1fb91055b95df4997a5aa42415ee63a6fa2186d4
asiddi29 <asiddi29@LAMU02FLGKEMD6M.uhc.com>
2024-09-02 18:29:26 +0530

    matplot-basic

--- data/external/data-vis/download_dataset.py
+++ data/external/data-vis/download_dataset.py
@@ -4,6 +4,15 @@ import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_and_extract(dataset, file_name, download_path, extract_path):
+    
+    target_file_path = os.path.join(extract_path, file_name)
+    
+    # Check if the file already exists in the target directory
+    if os.path.exists(target_file_path):
+        print(f"{file_name} already exists in {extract_path}. Skipping download.")
+        return
+    
+    
     api = KaggleApi()
     api.authenticate()
     
@@ -24,11 +33,17 @@ def download_and_extract(dataset, file_name, download_path, extract_path):
         if os.path.exists(csv_file_path):
             shutil.move(csv_file_path, extract_path)
 
-# Parameters
-dataset = "alexisbcook/data-for-datavis"
-file_name = "fifa.csv"
-download_path = "./"
-extract_path = "data/external/data-vis/"
+# download fifa data sets
+download_and_extract(dataset="alexisbcook/data-for-datavis", file_name="fifa.csv", download_path="./", extract_path="data/external/data-vis/")
 
-# Execute the function
-download_and_extract(dataset, file_name, download_path, extract_path)
+# download spotify data sets
+download_and_extract(dataset="alexisbcook/data-for-datavis", file_name="spotify.csv", download_path="./", extract_path="data/external/data-vis/")
+
+# download flight_delays data sets
+download_and_extract(dataset="alexisbcook/data-for-datavis", file_name="flight_delays.csv", download_path="./", extract_path="data/external/data-vis/")
+
+# download insurance data sets
+download_and_extract(dataset="alexisbcook/data-for-datavis", file_name="insurance.csv", download_path="./", extract_path="data/external/data-vis/")
+
+# download iris data sets
+download_and_extract(dataset="alexisbcook/data-for-datavis", file_name="iris.csv", download_path="./", extract_path="data/external/data-vis/")
--- data/external/wine-reviews/download_dataset.py
+++ data/external/wine-reviews/download_dataset.py
@@ -4,6 +4,15 @@ import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_and_extract(dataset, file_name, download_path, extract_path):
+    
+    target_file_path = os.path.join(extract_path, file_name)
+    
+    # Check if the file already exists in the target directory
+    if os.path.exists(target_file_path):
+        print(f"{file_name} already exists in {extract_path}. Skipping download.")
+        return
+    
+    
     api = KaggleApi()
     api.authenticate()
     
@@ -23,12 +32,5 @@ def download_and_extract(dataset, file_name, download_path, extract_path):
         # Move the CSV file if the ZIP file is not present
         if os.path.exists(csv_file_path):
             shutil.move(csv_file_path, extract_path)
-
-# Parameters
-dataset = "zynicide/wine-reviews"
-file_name = "winemag-data-130k-v2.csv"
-download_path = "./"
-extract_path = "data/external/wine-reviews/"
-
 # Execute the function
-download_and_extract(dataset, file_name, download_path, extract_path)
+download_and_extract(dataset="zynicide/wine-reviews", file_name="winemag-data-130k-v2.csv", download_path="./", extract_path="data/external/wine-reviews/")
